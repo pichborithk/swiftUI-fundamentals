1f672612e1c685ec78cc5ee06fa16524159e32c9
Pichborith Kong <pichborith@gmail.com>
2024-09-17 17:22:34 -0700

    initial weather app project

---
+++ 1- Weather App/SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Pichborith Kong on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                    
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}

8d59f4cdb6814b4df4b62f2de2292fc93af754ee
Pichborith Kong <pichborith@gmail.com>
2024-09-17 17:20:52 -0700

    add git ignore

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Pichborith Kong on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

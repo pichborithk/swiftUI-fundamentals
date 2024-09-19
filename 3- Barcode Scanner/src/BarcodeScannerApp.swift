da587f59d7510e54d1babf8e56351dd5c337c272
Pichborith Kong <pichborith@gmail.com>
2024-09-19 09:25:07 -0700

    initialize barcode scanner project

---
+++ 3- Barcode Scanner/src/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Pichborith Kong on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ 3- Barcode Scanner/src/BarcodeScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Pichborith Kong on 9/19/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}

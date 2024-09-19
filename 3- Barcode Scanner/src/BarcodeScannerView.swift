90b2ec4409aca00b979496d97cc74e29febc75e8
Pichborith Kong <pichborith@gmail.com>
2024-09-19 14:04:32 -0700

    handling error with alert

--- 3- Barcode Scanner/src/BarcodeScannerView.swift
+++ 3- Barcode Scanner/src/BarcodeScannerView.swift
@@ -12,21 +12,21 @@ import SwiftUI
 }
 
 struct BarcodeScannerView: View {
-    
     @State private var scannedCode: String?
-    
+    @State private var alertItem: AlertItem?
+
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $scannedCode)
+                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
-                
+
                 Spacer()
                     .frame(height: 64)
-                
+
                 Label("Scan Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
-                
+
                 Text(scannedCode ?? "Not Yet Scanned")
                     .bold()
                     .font(.largeTitle)
@@ -34,8 +34,26 @@ struct BarcodeScannerView: View {
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: Text(alertItem.title), message: Text(alertItem.message), dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
 
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
 
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something is wrong with the camera. We are unable to capture the input.",
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Scan Type",
+                                              message: "The value scanned is not valid. This app scans EAN-8 and EA-13",
+                                              dismissButton: .default(Text("OK")))
+}

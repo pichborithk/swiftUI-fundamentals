26deb160634ebb919c1d164165b80f46055add9e
Pichborith Kong <pichborith@gmail.com>
2024-09-19 14:56:05 -0700

    initialize appetizers project

---
+++ 4- Appetizers/src/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Pichborith Kong on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

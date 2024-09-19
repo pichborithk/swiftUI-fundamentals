e302457e42242efc3d52283d5c615f72da900f13
Pichborith Kong <pichborith@gmail.com>
2024-09-19 09:57:17 -0700

    build ui for barcode scanner view

--- 3- Barcode Scanner/src/BarcodeScannerView.swift
+++ 3- Barcode Scanner/src/BarcodeScannerView.swift
@@ -7,12 +7,32 @@
 
 import SwiftUI
 
-struct BarcodeScannerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
 #Preview {
     BarcodeScannerView()
 }
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 64)
+                
+                Label("Scan Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+

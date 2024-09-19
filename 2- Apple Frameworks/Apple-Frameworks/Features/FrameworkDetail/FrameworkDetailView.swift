bebada9a703855e91f0cea7c081931593a5a19a1
Pichborith Kong <pichborith@gmail.com>
2024-09-18 23:02:03 -0700

    example of apple default button style in Ios 15

--- 2- Apple Frameworks/Apple-Frameworks/Features/FrameworkDetail/FrameworkDetailView.swift
+++ 2- Apple Frameworks/Apple-Frameworks/Features/FrameworkDetail/FrameworkDetailView.swift
@@ -33,8 +33,12 @@ struct FrameworkDetailView: View {
             Button {
                 isShowingSafariView = true
             } label: {
-                AFButton(title: "Learn More")
+//                AFButton(title: "Learn More")
+                Label("Learn More", systemImage: "book.fill")
             }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            .tint(.red)
         }
         .padding()
 //        .sheet(isPresented: $isShowingSafariView) {

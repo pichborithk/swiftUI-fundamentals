6c187b05780fe582557adc8e756f70ca032ad0ee
Pichborith Kong <pichborith@gmail.com>
2024-09-22 15:32:36 -0700

    add example for exten color from asset with older version

---
+++ 4- Appetizers/src/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  Appetizers
+//
+//  Created by Pichborith Kong on 9/22/24.
+//
+
+import SwiftUI
+
+// New version we dont need this anymore
+
+//extension Color {
+//    static let brandPrimary = Color("brandPrimary")
+//}
+//
+//extension UIColor {
+//    static let brandPrimary = UIColor(named: "brandPrimary")
+//}

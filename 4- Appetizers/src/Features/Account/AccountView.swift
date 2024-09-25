42c86c9ed948504063abd5b8d5cf4f1ec5ef22a7
Pichborith Kong <pichborith@gmail.com>
2024-09-24 19:40:33 -0700

    add focus state to navigate through form by keyboard, and add dismiss button on keyboard

--- 4- Appetizers/src/Features/Account/AccountView.swift
+++ 4- Appetizers/src/Features/Account/AccountView.swift
@@ -13,17 +13,40 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject var viewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
+
+    enum FormTextField {
+        case firstName, lastName, email
+    }
 
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Personal Information")) {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit {
+                            focusedTextField = .lastName
+                        }
+                        .submitLabel(.next)
+
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit {
+                            focusedTextField = .email
+                        }
+                        .submitLabel(.next)
+
                     TextField("Email", text: $viewModel.user.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit {
+                            focusedTextField = nil
+                        }
+                        .submitLabel(.continue)
+
                     DatePicker("Birthday", selection: $viewModel.user.birthDate, displayedComponents: .date)
 
                     Button {
@@ -41,8 +64,12 @@ struct AccountView: View {
                 }
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
-
             .navigationTitle("😀 Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Dismiss") { focusedTextField = nil }
+                }
+            }
         }
         .onAppear(perform: viewModel.retrieveUser)
         .alert(item: $viewModel.alertItem) { alertItem in
